04412c992cbd27a4e98d5b13da758e6a0093fd63
Ian Ramirez <Ian.Espinosa@jala.university>
2024-10-20 20:47:21 -0600

    feat: Add Cors Configuration

--- Web.Api/DependencyInjection.cs
+++ Web.Api/DependencyInjection.cs
@@ -1,26 +1,22 @@
+using Web.Api.Extensions;
 using Web.Api.Middlewares;
 
 namespace Web.Api;
 
-public static class DependencyInjection {
-  public static IServiceCollection
-  AddPresentation(this IServiceCollection services,
-                  IConfiguration configuration) {
-    services.AddControllers();
-    services.AddEndpointsApiExplorer();
-    services.AddSwaggerGen();
-    services.AddTransient<HandleErrors>();
+public static class DependencyInjection
+{
+    public static IServiceCollection
+    AddPresentation(this IServiceCollection services,
+                    IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddTransient<HandleErrors>();
 
-    services.AddCors(options => {
-      options.AddPolicy("SpecificOriginPolicy", builder => {
-        builder.WithOrigins("Cors:AllowedOrigin")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-      });
-    });
+        services.AddCustomCors(configuration);
+        services.AddJwtAuthentication(configuration);
 
-    services.AddJwtAuthentication(configuration);
-
-    return services;
-  }
+        return services;
+    }
 }
