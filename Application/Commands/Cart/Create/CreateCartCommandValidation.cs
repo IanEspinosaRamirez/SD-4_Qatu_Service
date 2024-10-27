297fec6700dccd4a083d85770271f6d54da9c6ae
finnisimo4768 <finnisimo4768@gmail.com>
2024-10-26 18:14:31 -0600

    feat: add validation create Cart Item

--- Application/Commands/Cart/Create/CreateCartCommandValidation.cs
+++ Application/Commands/Cart/Create/CreateCartCommandValidation.cs
@@ -2,10 +2,7 @@ using FluentValidation;
 
 namespace Application.Commands.Cart.Create;
 
-internal sealed class CreateCartCommandValidation : AbstractValidator<CreateCartCommand>
-{
-    public CreateCartCommandValidation()
-    {
-        RuleFor(x => x.UserId).NotEmpty();
-    }
+internal sealed class CreateCartCommandValidation
+    : AbstractValidator<CreateCartCommand> {
+  public CreateCartCommandValidation() { RuleFor(x => x.UserId).NotEmpty(); }
 }
