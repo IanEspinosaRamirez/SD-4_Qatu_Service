3a939cbc257f26945f300f96ba4f2e2095b64e37
Ian Ramirez <Ian.Espinosa@jala.university>
2024-10-28 02:24:50 -0600

    feat: add GetById Handler to Photo Controller

--- Application/Commands/Photo/GetById/GeyByIdPhotoQueryHandler.cs
+++ Application/Commands/Photo/GetById/GeyByIdPhotoQueryHandler.cs
@@ -6,47 +6,45 @@ using MediatR;
 
 namespace Application.Queries.Photo.GetPhotoUrl;
 
-internal sealed class GetByIdPhotoQueryHandler
-    : IRequestHandler<GetByIdPhotoQuery, ErrorOr<string>>
+internal sealed class GetByIdPhotoQueryHandler : IRequestHandler<GetByIdPhotoQuery, ErrorOr<string>>
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly StorageClient _storageClient;
     private const string BucketName = "finnisimoqatu";
 
-    public GetByIdPhotoQueryHandler(IUnitOfWork unitOfWork,
-                                    StorageClient storageClient)
+    public GetByIdPhotoQueryHandler(IUnitOfWork unitOfWork, StorageClient storageClient)
     {
         _unitOfWork = unitOfWork;
         _storageClient = storageClient;
     }
 
-    public async Task<ErrorOr<string>>
-    Handle(GetByIdPhotoQuery command, CancellationToken cancellationToken)
+    public async Task<ErrorOr<string>> Handle(
+        GetByIdPhotoQuery command,
+        CancellationToken cancellationToken
+    )
     {
-        var photo = await _unitOfWork.PhotoRepository.GetById(
-            new CustomerId(command.PhotoId));
+        var photo = await _unitOfWork.PhotoRepository.GetById(new CustomerId(command.PhotoId));
         if (photo == null)
         {
             return Error.NotFound("Photo.NotFound", "Photo not found.");
         }
 
+        // Obtener el nombre del objeto en el bucket
         var objectName = photo.ImageURL.Replace(
-            $"https://storage.googleapis.com/{BucketName}/", "");
+            $"https://storage.googleapis.com/{BucketName}/",
+            ""
+        );
 
-        await _storageClient.UpdateObjectAsync(
-            new Google.Apis.Storage.v1.Data.Object
-            {
-                Bucket = BucketName,
-                Name = objectName,
-                Acl =
-                  new List<Google.Apis.Storage.v1.Data.ObjectAccessControl> {
-                new Google.Apis.Storage.v1.Data.ObjectAccessControl {
-                  Entity = "allUsers", Role = "READER"
-                }
-                  }
-            });
+        // Generar una URL firmada con expiración de 3 horas
+        UrlSigner urlSigner = UrlSigner.FromServiceAccountPath(
+            "/home/acer/key/chromatic-being-438520-e5-309b0479f37e.json"
+        ); // Ruta a tu JSON de cuenta de servicio
+        var signedUrl = urlSigner.Sign(
+            BucketName,
+            objectName,
+            TimeSpan.FromHours(3) // Expiración de 3 horas
+        );
 
-        var publicUrl = $"https://storage.googleapis.com/{BucketName}/{objectName}";
-        return publicUrl;
+        return signedUrl;
     }
 }
