a0b08b8cfc6e09a8eae8de381d8a978a15963506
finnisimo4768 <finnisimo4768@gmail.com>
2024-11-06 16:39:37 -0600

    feat: refactor Cart

--- Application/Commands/Cart/GetById/GetByIdCartCommand.cs
+++
@@ -1,7 +0,0 @@
-using Application.Commands.Cart.Dto;
-using ErrorOr;
-using MediatR;
-
-namespace Application.Commands.Cart.GetById;
-
-public record GetByIdCartCommand(Guid Id) : IRequest<ErrorOr<ResponseGetCartByIdDto>>;
--- Application/Queries/Cart/DTOs/ResponseGetCartByIdDto.cs
+++ Application/Queries/Cart/DTOs/ResponseGetCartByIdDto.cs
@@ -1,4 +1,4 @@
-namespace Application.Commands.Cart.Dto;
+namespace Application.Queries.Cart.Dto;
 
 public class ResponseGetCartByIdDto
 {
